902431df1b1bcabed7c5ce9c9bcfbd33266b8f85
Jhorman Alexander Culma Hermosa <119307144+Alex022315@users.noreply.github.com>
2024-04-01 18:42:31 -0500

    Update usuarios.service.ts

--- src/src/app/servicios/Usuarios/usuarios.service.ts
+++ src/src/app/servicios/Usuarios/usuarios.service.ts
@@ -18,7 +18,7 @@ import { ProductoMateriaPrimaModel } from 'src/app/Modelos/Producto_Materia_Prim
   providedIn: 'root'
 })
 export class UsuariosService {
-  url = 'http://localhost:3000';
+  url = 'https://backendsimp-production-a5ff.up.railway.app';
 
   constructor(private http: HttpClient) { }
 
