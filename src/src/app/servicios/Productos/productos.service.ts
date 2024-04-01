e02b84459b04f8643c8bb6d61502e9b3a1d7acfb
Jhorman Alexander Culma Hermosa <119307144+Alex022315@users.noreply.github.com>
2024-04-01 18:42:09 -0500

    Update productos.service.ts

--- src/src/app/servicios/Productos/productos.service.ts
+++ src/src/app/servicios/Productos/productos.service.ts
@@ -9,7 +9,7 @@ import { ProductoMateriaModel } from 'src/app/Modelos/Producto_Materia.model';
   providedIn: 'root'
 })
 export class ProductosService {
-  url = 'http://localhost:3000';
+  url = 'https://backendsimp-production-a5ff.up.railway.app';
 
   constructor(private http: HttpClient) { }
 
